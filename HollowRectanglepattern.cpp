950e8980fa96ad1079ec1d25c63062893a1d778c
Dharmendra Pandit <99905700+dkrpandit@users.noreply.github.com>
2022-10-06 17:28:11 +0530

    Update HollowRectanglepattern.cpp

--- HollowRectanglepattern.cpp
+++ HollowRectanglepattern.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main(){
     
     int row, column;
-    cout<<"Enter the number rows: "<<endl;
+    cout<<"Enter the number rows: "<<"\n";
     cin>>row;
-    cout<<"Enter the number columns: "<<endl;
+    cout<<"Enter the number columns: "<<"\n";
     cin>>column;
 
     for(int i=1;i<=row;i++){
         for(int j=1;j<=column;j++){
             if(i==1 || i==row || j==1 || j==column){
-                cout<<"* ";
+                cout<<"*  ";
             }
             else{
-                cout<<"  ";
+                cout<<"   ";
             }
         }
-        cout<<endl;
+        cout<<"\n";
     }
 }
