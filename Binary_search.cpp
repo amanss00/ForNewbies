2dee62b2392d1edca9881d5a39c123a1f8be5331
mahek350 <117099185+mahek350@users.noreply.github.com>
2022-10-31 20:36:43 +0530

    Update Binary_search.cpp

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int Arr[15] = {6,8,13,17,20,22,25,28,30,35}, low = 0 , high = 9 , mid , srch;
-    cout << "Enter the Element to search : ";
+    cout<<"Enter the Element to search : ";
     cin >> srch;
 
     while(low <= high)
@@ -12,8 +12,8 @@ int main()
         mid = (low + high)/2;
         if(srch == Arr[mid])
         {
-            cout << "Search successful." << endl;
-            cout << "The Element is found at " << mid << endl;
+            cout<< "Search successful." << endl;
+            cout<< "The Element is found at " << mid << endl;
             return 0;
         }
         else if(srch < Arr[mid])
