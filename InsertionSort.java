c46ee8a7ed1832ee667a548bbe1edbb1eb2bba6a
mahek350 <117099185+mahek350@users.noreply.github.com>
2022-10-31 20:39:04 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -5,7 +5,7 @@ class Sorting{
 
     public static int[] insertionSort(int[] array, int nElements){
         for(int i=1; i<nElements; i++){ // outer loop starts from 1 as we get 0th index as the first sorted array
-            int temp = array[i]; // assigning first element of the unsorted subarray to temp variable
+            int temp=array[i]; // assigning first element of the unsorted subarray to temp variable
 
             //inner loop - decrementing - comparisons between sorted subarray and value in temp variable
             int j = i-1; // sorted sub array is just before unsorted subarray
@@ -23,8 +23,8 @@ class Sorting{
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] testArray = {12, 5, 63, 47, 12, 4};
-        Sorting sortObj = new Sorting();
+        int[] testArray= {12, 5, 63, 47, 12, 4};
+        Sorting sortObj= new Sorting();
 
 
         int[] insertionSortedArray = sortObj.insertionSort(testArray, testArray.length);
